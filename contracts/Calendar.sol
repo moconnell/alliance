c539e4cfcecc8ac9f7d2df3b01ec93941489a060
Zach Kolodny <kolodnyz@icloud.com>
2021-08-26 22:28:54 -0400

    init

---
+++ contracts/Calendar.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.3;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@quant-finance/solidity-datetime/contracts/DateTime.sol";
+
+contract Calendar is Ownable {
+    using SafeMath for uint256;
+
+    enum Day {
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY
+    }
+
+    int8 public timezone;
+    string private emailAddress;
+    mapping(Day => bool) public availableDays;
+    uint256 public availableStartTime;
+    uint256 public availableEndTime;
+    uint256 public duration;
+
+    mapping(uint256 => address) public meetingSchedule;
+
+    event MeetingBooked(
+        address indexed _attendee,
+        uint256 _meetingDatetime
+    );
+
+    constructor(
+        int8 _timezone,
+        string memory _emailAddress,
+        address newOwner,
+        Day[] memory _availableDays,
+        uint256 _availableStartTime,
+        uint256 _availableEndTime,
+        uint256 _duration
+    )
+    {
+        timezone = _timezone;
+        emailAddress = _emailAddress;
+
+        for (uint i = 0; i < _availableDays.length; i++) {
+            availableDays[_availableDays[i]] = true;
+        }
+
+        availableStartTime = _availableStartTime;
+        availableEndTime = _availableEndTime;
+        duration = _duration;
+
+        transferOwnership(newOwner);
+    }
+
+    function bookMeeting(uint256 _datetime) external {
+        (, , , uint256 hr, uint256 min, ) = DateTime.timestampToDateTime(_datetime);
+        uint256 formattedTime = (hr * 100) + min;
+        require(formattedTime >= availableStartTime, "bookMeeting::cant book before start time.");
+        require(formattedTime < availableEndTime, "bookMeeting::cant book after end time.");
+        require(meetingSchedule[_datetime] == address(0), "bookMeeting::time already booked."); 
+        require(_datetime > block.timestamp, "bookMeeting::cant book in past");
+
+        // ToDo: Add day check
+
+        meetingSchedule[_datetime] = msg.sender;
+
+        emit MeetingBooked(msg.sender, _datetime);
+    }
+
+    function cancelMeeting(uint256 _datetime) external {
+        require(meetingSchedule[_datetime] == msg.sender, "cancelMeeting::cant cancel meeting that isnt yours."); 
+        meetingSchedule[_datetime] = address(0);
+
+        // ToDo: Emit event
+    }
+
+    function withdraw() external {
+        address beneficiary = owner();
+        uint256 balance = address(this).balance;
+        (bool success, bytes memory returnData) = beneficiary.call{
+            value: balance
+        }("");
+        require(success, string(returnData));
+    }
+
+    receive () external payable { }
+}
