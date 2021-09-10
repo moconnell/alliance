d6a6b59e8ad9c0dc562980d64191f2b1f8eca9ad
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2021-09-10 09:12:56 +0200

    Refactored test.

--- contracts/CalendarFactory.sol
+++ contracts/CalendarFactory.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "./Calendar.sol";
-import "hardhat/console.sol";
 
 contract CalendarFactory {
 
--- test/calendar-test.ts
+++ test/calendar-test.ts
@@ -23,57 +23,65 @@ const bobsCalendarConfig = {
   startTime : [8, 0], // 8:00
   endTime : [16, 30], // 16:30
 }
+async function prepareCalendarFactory(deployer: Signer) {
+  // deploy CalendarLib
+  const CalendarLib = await ethers.getContractFactory("CalendarLib");
+  let calendarLib = await CalendarLib.deploy();
+
+  // deploy CalendarFactory
+  const CalendarFactory = await ethers.getContractFactory("CalendarFactory", {
+    libraries: {
+      CalendarLib: calendarLib.address
+    } });
+
+  return [await CalendarFactory.deploy(), calendarLib];
+}
+
+async function prepareCalendar(calendarFactory: Contract, signer: Signer, config: Object) {
+
+  let tx: ContractTransaction = await calendarFactory.connect(signer).createCalendar(
+    ...Object.values(config)
+  );
+  let receipt: ContractReceipt = await tx.wait();
+  const txEvent = receipt.events?.[0]?.args;
+  let userAddress = txEvent?.userAddress;
+  let calenderAddress = txEvent?.calenderAddress;
+  let id = txEvent?.id;
+
+  chai.expect(userAddress).to.equal(await signer.getAddress());
+  const calendarAddressFromMapping = await calendarFactory.calendarIdToCalendar(id);
+  chai.expect(calenderAddress).to.equal(calendarAddressFromMapping);
+
+  return [await ethers.getContractAt("Calendar", calenderAddress), id];
+}
 
 
 describe("CalendarFactory", function() {
-  let calendarLib: Contract;
-  let calendarFactory : Contract;
-  let alicesCalendar: Contract;
-  let bobsCalendar: Contract;
+  let calendarFactory: Contract;
+  let cal1: Contract;
+  let cal2: Contract;
+  let alice: Signer, bob: Signer;
 
   beforeEach(async function() {
-    const [alice, bob]  = await ethers.getSigners();
+    [alice, bob]  = await ethers.getSigners();
 
-    async function prepareCalendar(calendarFactory: Contract, signer: Signer, config: Object) {
-      let tx: ContractTransaction = await calendarFactory.connect(signer).createCalendar(...Object.values(config));
-      let receipt: ContractReceipt = await tx.wait();
-      const txEvent = receipt.events?.[0]?.args;
-      let userAddress = txEvent?.userAddress;
-      let calenderAddress = txEvent?.calenderAddress;
-      let id = txEvent?.id;
-
-      chai.expect(userAddress).to.equal(await signer.getAddress());
-      const calendarAddressFromMapping = await calendarFactory.calendarIdToCalendar(id);
-      chai.expect(calenderAddress).to.equal(calendarAddressFromMapping);
-
-      return [await ethers.getContractAt("Calendar", calenderAddress), id];
-    }
-
-    // deploy CalendarLib
-    const CalendarLib = await ethers.getContractFactory("CalendarLib");
-    calendarLib = await CalendarLib.deploy();
-
-    // deploy CalendarFactory
-    const CalendarFactory = await ethers.getContractFactory("CalendarFactory", {
-      libraries: {
-        CalendarLib: calendarLib.address
-      } });
-    calendarFactory = await CalendarFactory.deploy();
+    [calendarFactory] = await prepareCalendarFactory(alice);
 
     // deploy alice's calendar
-    let alicesCalendarId;
-    [alicesCalendar, alicesCalendarId] = await prepareCalendar(calendarFactory, alice, alicesCalendarConfig);
-    chai.expect(alicesCalendarId).to.equal(0);
+    let cal1Id;
+    [cal1, cal1Id] = await prepareCalendar(calendarFactory, alice, alicesCalendarConfig);
+    chai.expect(cal1Id).to.equal(0);
 
     // deploy bobs calendar
-    let bobsCalendarId;
-    [bobsCalendar, bobsCalendarId] = await prepareCalendar(calendarFactory, bob, bobsCalendarConfig);
-    chai.expect(bobsCalendarId).to.equal(1);
+    let cal2Id;
+    [cal2, cal2Id] = await prepareCalendar(calendarFactory, bob, bobsCalendarConfig);
+    chai.expect(cal2Id).to.equal(1);
+
   });
 
   it("should initialize calendars correctly", async function() {
-    chai.expect(await alicesCalendar.timezone()).to.equal(alicesCalendarConfig.timezone);
-    chai.expect(await bobsCalendar.timezone()).to.equal(bobsCalendarConfig.timezone);
+    chai.expect(await cal1.timezone()).to.equal(alicesCalendarConfig.timezone);
+    chai.expect(await cal2.timezone()).to.equal(bobsCalendarConfig.timezone);
   });
 
 });
