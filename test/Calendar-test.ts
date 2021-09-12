522927b9bf64811cf3b3eb5cd844c23016028e10
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2021-09-12 02:44:41 +0200

    Refactored test to work also in the next years.
    Because dates should be in the future, the test year must be dynamic. However, tests will fail in the afternoon of December 31.

--- test/Calendar-test.ts
+++ test/Calendar-test.ts
@@ -11,6 +11,8 @@ describe("Calendar", function() {
   let cal1: Contract, cal2: Contract, cal3: Contract;
   let signer1: Signer, signer2: Signer, signer3: Signer;
 
+  const year = new Date().getFullYear(), month = 12, day = 31;
+
   beforeEach(async function() {
     [signer1, signer2, signer3] = await ethers.getSigners();
     calendarFactory = await deployCalendarFactory(signer1);
@@ -22,14 +24,14 @@ describe("Calendar", function() {
   it("books meetings with others within the available hours", async function() {
     const signer1Address = await signer1.getAddress();
 
-    const res0 = await cal2.getMeetings(2021, 12, 31);
+    const res0 = await cal2.getMeetings(year, month, 31);
     chai.expect(res0).to.be.instanceof(Array);
     chai.expect(res0).to.have.length(0);
 
     const [hour1, min1] = [14, 15], duration = 60;
 
-    await cal2.connect(signer1).bookMeeting(2021, 12, 31, hour1, min1, duration);
-    const res1 = await cal2.getMeetings(2021, 12, 31);
+    await cal2.connect(signer1).bookMeeting(year, month, day, hour1, min1, duration);
+    const res1 = await cal2.getMeetings(year, month, 31);
 
     chai.expect(res1).to.be.instanceof(Array);
     chai.expect(res1).to.have.length(1);
@@ -40,8 +42,8 @@ describe("Calendar", function() {
     chai.expect(res1[0].duration).to.deep.equal(duration);
 
     const [hour2, min2] = [15, 30];
-    await cal2.connect(signer1).bookMeeting(2021, 12, 31, hour2, min2, duration);
-    const res2 = await cal2.getMeetings(2021, 12, 31);
+    await cal2.connect(signer1).bookMeeting(year, month, day, hour2, min2, duration);
+    const res2 = await cal2.getMeetings(year, month, 31);
     chai.expect(res2).to.be.instanceof(Array);
     chai.expect(res2).to.have.length(2);
 
@@ -58,33 +60,33 @@ describe("Calendar", function() {
 
     it("cancels owned meetings", async function() {
       const [hour, min] = [14, 15];
-      await cal2.connect(signer1).bookMeeting(2021, 12, 31, hour, min, 60);
-      await cal2.connect(signer1).cancelMeeting(2021, 12, 31, 0);
+      await cal2.connect(signer1).bookMeeting(year, month, day, hour, min, 60);
+      await cal2.connect(signer1).cancelMeeting(year, month, day, 0);
 
-      const res = await cal2.getMeetings(2021, 12, 31);
+      const res = await cal2.getMeetings(year, month, 31);
       chai.expect(res).to.be.instanceof(Array);
       chai.expect(res).to.be.empty;
     });
 
     it("reverts on cancelling non-existing meetings", async function() {
       let id = 0;
-      await chai.expect(cal2.connect(signer1).cancelMeeting(2021, 12, 31, id)
+      await chai.expect(cal2.connect(signer1).cancelMeeting(year, month, day, id)
       ).to.be.revertedWith("Meeting does not exist.");
 
-      cal2.connect(signer1).bookMeeting(2021, 12, 31, 14, 15, 60);
+      cal2.connect(signer1).bookMeeting(year, month, day, 14, 15, 60);
 
       id = 1;
-      await chai.expect(cal2.connect(signer1).cancelMeeting(2021, 12, 31, id)
+      await chai.expect(cal2.connect(signer1).cancelMeeting(year, month, day, id)
       ).to.be.revertedWith("Meeting does not exist.");
     });
 
   it("reverts on invalid date and time", async function() {
     await chai.expect(
-      cal2.connect(signer1).bookMeeting(2021, 2, 29, 12, 0, 60)
+      cal2.connect(signer1).bookMeeting(year, 2, 29, month, 0, 60)
     ).to.be.revertedWith("Date and time are not valid.");
 
     await chai.expect(
-      cal2.connect(signer1).bookMeeting(2021, 2, 28, 24, 0, 10)
+      cal2.connect(signer1).bookMeeting(year, 2, 28, 24, 0, 10)
     ).to.be.revertedWith("Date and time are not valid.");
   });
 
@@ -104,60 +106,60 @@ describe("Calendar", function() {
     });
 
     it("prohibits to book a meeting overlapping with an earlier meeting", async function() {
-      await cal1.connect(signer2).bookMeeting(2021, 12, 31, 14, 0, 60);
+      await cal1.connect(signer2).bookMeeting(year, month, day, 14, 0, 60);
       await chai.expect(
-        cal1.connect(signer3).bookMeeting(2021, 12, 31, 14, 30, 60)
+        cal1.connect(signer3).bookMeeting(year, month, day, 14, 30, 60)
       ).to.be.revertedWith("Overlap with existing meeting.");
     });
 
     it("prohibits to book a meeting overlapping with a later meeting", async function() {
-      await cal1.connect(signer2).bookMeeting(2021, 12, 31, 14, 0, 60);
+      await cal1.connect(signer2).bookMeeting(year, month, day, 14, 0, 60);
       await chai.expect(
-        cal1.connect(signer3).bookMeeting(2021, 12, 31, 13, 30, 60)
+        cal1.connect(signer3).bookMeeting(year, month, day, 13, 30, 60)
       ).to.be.revertedWith("Overlap with existing meeting.");
     });
 
     it("prohibits to book a meeting falling inside of another meeting", async function() {
-      await cal1.connect(signer2).bookMeeting(2021, 12, 31, 14, 0, 60);
+      await cal1.connect(signer2).bookMeeting(year, month, day, 14, 0, 60);
       await chai.expect(
-        cal1.connect(signer3).bookMeeting(2021, 12, 31, 14, 20, 20)
+        cal1.connect(signer3).bookMeeting(year, month, day, 14, 20, 20)
       ).to.be.revertedWith("Overlap with existing meeting.");
     });
 
     it("prohibits to book a meeting surrounding another meeting", async function() {
-      await cal1.connect(signer2).bookMeeting(2021, 12, 31, 14, 0, 60);
+      await cal1.connect(signer2).bookMeeting(year, month, day, 14, 0, 60);
       await chai.expect(
-        cal1.connect(signer3).bookMeeting(2021, 12, 31, 13, 0, 180)
+        cal1.connect(signer3).bookMeeting(year, month, day, 13, 0, 180)
       ).to.be.revertedWith("Overlap with existing meeting.");
     });
 
     it("prohibits to book a meeting that overlaps with a meeting from the previous day", async function() {
-      await cal3.connect(signer2).bookMeeting(2021, 12, 31, 23, 0, 120);
+      await cal3.connect(signer2).bookMeeting(year, month, day, 23, 0, 120);
       await chai.expect(
-        cal3.connect(signer2).bookMeeting(2022, 1, 1, 0, 30, 60)
+        cal3.connect(signer2).bookMeeting(year+1, 1, 1, 0, 30, 60)
       ).to.be.revertedWith("Overlap with existing meeting on previous day.");
     });
 
   it("prohibits to book a meeting that overlaps with a meeting from the next day", async function() {
-    await cal3.connect(signer2).bookMeeting(2022, 1, 1, 0, 30, 60);
+    await cal3.connect(signer2).bookMeeting(year+1, 1, 1, 0, 30, 60);
 
     await chai.expect(
-      cal3.connect(signer2).bookMeeting(2021, 12, 31, 23, 0, 120)
+      cal3.connect(signer2).bookMeeting(year, month, day, 23, 0, 120)
     ).to.be.revertedWith("Overlap with existing meeting on next day.");
   });
 
     it("prohibits cancelling meetings of others", async function() {
-      await cal2.connect(signer1).bookMeeting(2021, 12, 31, 14, 15, 60);
+      await cal2.connect(signer1).bookMeeting(year, month, day, 14, 15, 60);
 
       let id = 0;
-      await chai.expect(cal2.connect(signer2).cancelMeeting(2021, 12, 31, id)
+      await chai.expect(cal2.connect(signer2).cancelMeeting(year, month, day, id)
       ).to.be.revertedWith(
         "You cannot cancel a meeting that you have not booked yourself.");
     });
 
     it("prohibits booking meetings with yourself", async function() {
       await chai.expect(
-        cal2.connect(signer2).bookMeeting(2021, 12, 31, 14, 15, 60)
+        cal2.connect(signer2).bookMeeting(year, month, day, 14, 15, 60)
       ).to.be.revertedWith("You cannot book a meeting with yourself.");
     });
 });
