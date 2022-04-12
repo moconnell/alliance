180779ba3b6f1fe59a11feb281bcefb1b1580cfd
Matthew O'Connell <totallyterrific@tutanota.com>
2022-04-12 13:54:54 +0200

    deploy CalendarFactory

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,23 +6,6 @@
 
 import { ethers } from "hardhat";
 
-const deployContract = async (name: string) => {
-  try {
-    console.log(`${name}: starting deployment`);
-
-    const fac = await ethers.getContractFactory(name);
-    console.log(`${name}: got contract factory`);
-
-    const contract = await fac.deploy();
-    console.log(`${name}: deployment in progress`);
-
-    await contract.deployed();
-    console.log(`${name}: deployed to ${contract.address}`);
-  } catch (error) {
-    console.error(`${name}: error`);
-  }
-};
-
 const main = async () => {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
@@ -34,25 +17,14 @@ const main = async () => {
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
-
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  // const contracts = ["Calendar"];
-  // contracts.forEach(async (name) => {
-  const name = "Calendar";
-  console.log(`${name}: starting deployment`);
-
+  const name = "CalendarFactory";
   const fac = await ethers.getContractFactory(name);
-  console.log(`${name}: got contract factory`);
-
   const contract = await fac.deploy();
-  console.log(`${name}: deployment in progress`);
-
   await contract.deployed();
-  console.log(`${name}: deployed to ${contract.address}`);
-  // });
 
-  console.log("Deployment finished");
+  console.log(`${name}: deployed to ${contract.address}`);
 };
 
 // We recommend this pattern to be able to use async/await everywhere
